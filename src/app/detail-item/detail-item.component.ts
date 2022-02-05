df544945e08c632e04c4b602b399c5c270e5d649
cesarartunduaga <cesar.artunduaga@sistemasexpertos.cl>
2022-02-05 04:09:35 -0300

    se comenzo con la maquetacion del detalle del item

--- src/app/detail-item/detail-item.component.ts
+++ src/app/detail-item/detail-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { AppBreadcrumbService } from '../app.breadcrumb.service';
 
 @Component({
   selector: 'app-detail-item',
@@ -8,8 +9,12 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class DetailItemComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute,) {
+  constructor(private route: ActivatedRoute,private breadcrumbService: AppBreadcrumbService) {
     console.log(this.route.snapshot.paramMap.get('id'));
+    this.breadcrumbService.setItems([
+        { label: 'Ui Kit' },
+        { label: 'Charts', routerLink: ['/uikit/charts'] }
+    ]);
    }
 
   ngOnInit(): void {
